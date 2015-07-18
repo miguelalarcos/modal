e43075cd0083dc44666c3a27629f9e2bd474474a
miguelalarcos <miguel.alarcos@gmail.com>
2015-07-18 02:59:20 +0200

    version 0.4.4

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'miguelalarcos:modal',
-  version: '0.4.3',
+  version: '0.4.4',
   summary: 'A modal package for Meteor.',
   git: 'https://github.com/miguelalarcos/modal.git',
   documentation: 'README.md'
